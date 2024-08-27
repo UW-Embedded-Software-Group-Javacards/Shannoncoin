8cd4bc4af94178d63d8c96ed5f5861a98c735ea0
Krish Shah <shahkrish2016@gmail.com>
2024-08-27 18:20:32 -0400

    initial commit

---
+++ src/test/java/javacard/washing_machine/WashingMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package javacard.washing_machine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WashingMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
