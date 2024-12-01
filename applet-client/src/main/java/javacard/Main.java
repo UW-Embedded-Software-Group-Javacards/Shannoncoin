c87fa743ad2c4c353b0c1bc11d1481e14726d0fc
Krish Shah <shahkrish2016@gmail.com>
2024-11-30 21:45:55 -0500

    finished `process`

--- applet-client/src/main/java/javacard/Main.java
+++ applet-client/src/main/java/javacard/Main.java
@@ -34,20 +34,60 @@ public class ShannonCard extends Applet {
         return keypair;
     }
 
-    // process method - runs when receives apdu
+    /*
+     * Handles incoming APDU commands and responds accordingly
+     */
     public void process(APDU apdu) {
+        byte[] buffer = apdu.getBuffer();
+        byte cla = apdu.getCLA();  // class byte to identify type of command
+        byte ins = apdu.getINS();  // instruction byte to identify command
+
+
         // divert from 2 specified commands to respond public key and sign data
+        if (cla == (byte) 0x00) {  // Only handle commands with CLA 0x00
+            if (ins == (byte) 0x01) {
+                // respond with the public key
+                respondPublicKey(pub);
+            } else if (ins == (byte) 0x02) {
+                // sign data with private key
+                //byte[] dataToSign = new byte[dataLength];  // Create a byte array for the data
+                //System.arraycopy(buffer, 0, dataToSign, 0, 32);
+                signData(buffer);
+            } else {
+                // error for unsupported instruction
+                ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
+            }
+        } else {
+            // error for unsupported command type
+            ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);
+        }
     }
 
     public void respondPublicKey() {
         PublicKey pub = this.keypair.getPublic();
+        // get the public key as a byte array
+        byte[] pubKeyBytes = pub.getEncoded();  
+        short pubKeyLength = (short) pubKeyBytes.length; 
+        
+        // setup apdu response
+        APDU.setOutgoing();
+        APDU.setOutgoingLength(pubKeyLength);
+        APDU.sendBytes((short) 0, pubKeyBytes, (short) 0, pubKeyLength);
     }
 
-    // data is 32 bytes
+    /*
+     * Sign data with private key for validation
+     */
     public void signData(byte[] data) {
+        // NOTE: data is 32 bytes
         PrivateKey priv = this.keypair.getPrivate();
+        byte[] signatureBuffer = new byte[256];
         short signatureLength = signer.sign(data, 0, this.dataLength, this.signatureBuffer, 0);
 
+        // setup apdu response
+        APDU.setOutgoing();
+        APDU.setOutgoingLength(signatureLength); 
+        APDU.sendBytes((short) 0, signature, (short) 0, signatureLength);
     }
 
 }
